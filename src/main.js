c4c0733b93faef66720c08a2da849ece3288a434
SWizzard779 <sofia.nesimko@gmail.com>
2024-03-25 14:44:30 +0100

    structure

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import css from './css/styles.css'
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import axisos from "axios";
